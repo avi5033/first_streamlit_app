20818097b709ea25aac78953afc97613e7363231
avi5033 <110692782+avi5033@users.noreply.github.com>
2022-08-12 16:49:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,9 @@ fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #to display th etable on the page
 streamlit.dataframe(fruits_to_show)
+
+
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
