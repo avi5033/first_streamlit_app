81b0478d70246456413183823a24fdbeb5e05774
avi5033 <110692782+avi5033@users.noreply.github.com>
2022-08-12 19:15:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,10 @@ my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit Load List Contains:")
+streamlit.header("The Fruit Load List Contains:")
 #streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 streamlit.title('My Parents New Healthy Diner')
