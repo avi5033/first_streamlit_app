507cabf77dfb65c073d914f42b6fc0b2b330a82b
avi5033 <110692782+avi5033@users.noreply.github.com>
2022-08-12 19:37:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Which Fruits Do you like to add?")
 
 add_my_fruit = streamlit.text_input('The user entered:','Kiwi')
-streamlit.write('The user entered ')
+streamlit.write('The user entered ',add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit )
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list_values values ('from streamlit')")
