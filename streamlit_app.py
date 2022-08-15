ec72f1e6a5d0a3d02e88a6b790296e3bb4f2d180
avi5033 <110692782+avi5033@users.noreply.github.com>
2022-08-15 11:40:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,21 +24,30 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #to display th etable on the page
 streamlit.dataframe(fruits_to_show)
 
+
+# repeatable code block called Function
+def get_frutyvise_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+#new section to display frutyvice response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
   #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:  
+      back_from_function = get_frutyvise_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
       #streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       #streamlit.text(fruityvice_response.json())
       # write your own comment -what does the next line do? 
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
-      streamlit.dataframe(fruityvice_normalized)
+      #streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
   
